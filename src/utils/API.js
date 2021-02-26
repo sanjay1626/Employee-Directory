44a25917da5649186243a8f6e7eb2693e585c5f5
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-02-25 21:03:25 -0800

    Deployed app

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const URL = "https://randomuser.me/api/?results=200&nat=us";
+
+export default {
+  getUsers: function(query) {
+    return axios.get(URL + query);
+  }
+};
+
+ 
+
